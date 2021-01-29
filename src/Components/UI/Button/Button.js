b7798f3af06f82bf67025af6fe68a24449562a5f
Swee Kwang Chua <sweekwangchua@SweeKwang.local>
2021-01-29 20:44:04 +0800

    Updates

--- src/Components/UI/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import classes from './Button.module.css';
-
-const Button = (props) => {
-    return(
-        <a href = {props.href} className = {classes.Button} onClick = {props.onClick}>{props.children}</a>
-    )
-};
-
-export default Button;
